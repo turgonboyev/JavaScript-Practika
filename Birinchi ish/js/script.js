b3065684644d330ec8f77dfb0f6b1781fa465361
turgonboyev <85232464+turgonboyev@users.noreply.github.com>
2021-08-12 00:17:11 +0500

    Birinchi ish yuklandi.

---
+++ Birinchi ish/js/script.js
@@ -0,0 +1,52 @@
+window.addEventListener('DOMContentLoaded' , function(){
+
+    let products = document.querySelectorAll('.product'), 
+        buttons = document.querySelectorAll('button'), 
+        openBtn = document.querySelector('.open');
+
+    function createCard(){
+        let cart = document.createElement('div'),
+            field = document.createElement('div'),
+            heading = document.createElement('h2'),
+            closeBtn = document.createElement('button');
+
+        cart.classList.add('cart');
+        field.classList.add('cart-field');
+        closeBtn.classList.add('close');
+
+        heading.textContent = 'В нашей корзине';
+        closeBtn.textContent = 'Закрыть';
+
+        document.body.appendChild(cart)
+        cart.appendChild(heading)
+        cart.appendChild(field)
+        cart.appendChild(closeBtn)
+    }
+
+    createCard()
+
+    let cart = document.querySelector('.cart'),
+        closeBtn = document.querySelector('.close'),
+        field = document.querySelector('.cart-field')
+
+    openBtn.addEventListener('click', function(){
+        cart.style.display = 'block'
+    })
+
+    closeBtn.addEventListener('click', function(){
+        cart.style.display = 'none'
+    })
+
+    buttons.forEach(function(item, i){
+        item.addEventListener('click', function(){
+            let cloneItem = products[i].cloneNode(true),
+                btn = cloneItem.querySelector('button')
+
+            btn.remove();
+            field.appendChild(cloneItem)
+            products[i].remove()
+
+        })
+    })
+
+})
