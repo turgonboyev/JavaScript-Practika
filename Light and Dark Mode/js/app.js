321fd6a55ce799063d33dede9ce68ab44f577af8
turgonboyev <botirturgonboyev2000@email.com>
2021-09-10 14:13:30 +0500

    Light and Dark Mode

---
+++ Light and Dark Mode/js/app.js
@@ -0,0 +1,20 @@
+'use strice';
+
+var checkbox = document.querySelector('input[name=theme]');
+
+checkbox.addEventListener('change', function() {
+  if(this.checked){
+    trans();
+    document.documentElement.setAttribute('data-theme', 'dark');
+  }else{
+    trans();
+    document.documentElement.setAttribute('data-theme', 'light');
+  }
+})
+
+function trans(){
+  document.documentElement.classList.add('transition');
+  window.setTimeout(() => {
+    document.documentElement.classList.remove('transition')
+  }, 1000)
+}
