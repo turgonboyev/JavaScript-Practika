4b42bea2a7576dac061444493d84a9e300fd86c4
turgonboyev <botirturgonboyev2000@email.com>
2021-09-10 07:57:44 +0500

    Iphone 12 responsive

---
+++ Iphone12 responsive/js/app.js
@@ -0,0 +1,12 @@
+'use strice';
+
+window.addEventListener('DOMContentLoaded', () => {
+  const loader = document.querySelector('.loader');
+
+  setTimeout(() => {
+    loader.style.opacity = 0;
+    setTimeout(() => {
+      loader.style.display = 'none';
+    }, 800)
+  }, 1800)
+})
